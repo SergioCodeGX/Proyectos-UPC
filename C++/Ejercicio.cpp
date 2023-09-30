ae3ea48c4b8948e4baa5f351123d093a3ba4ce68
SergioDevEngineer <132532322+SergioCodeGX@users.noreply.github.com>
2023-09-30 03:38:21 +0000

    Cambios

--- C++/Ejercicio.cpp
+++ C++/Ejercicio.cpp
@@ -3,31 +3,80 @@ using namespace std;
 
 int leerNumeroClientes();
 void mostrarMenuOpciones();
-float calcularMonto();
-void mostrarResultado();
+int leerCantidadVideoJuegos();
+int leerCantidadAccesorios();
+int leerCantidadTarjetas();
+float calcularMonto(int cantidadVideoJuegos, int cantidadAccesorios, int cantidadTarjetas);
+void reporteDeVentas(int uni_videojuegos, int uni_accesorios, int uni_tarjetas, float monto_total, int montoDescontado);
 
 int main(){
-    int cliente;
+
+    int cantidadVideoJuegos;
+    int cantidadAccesorios;
+    int cantidadTarjetas;
+    int contadorClientes = 0;
+    int contadorVideoJuegos = 0;
+    int contadorAccesorios = 0;
+    int contadorTarjetas = 0;
+    float montoTotal = 0;
+    int numeroClientes = leerNumeroClientes();
+
+    do
+    {
+       cout << "Cliente " << contadorClientes + 1 << endl;
+       cantidadVideoJuegos = leerCantidadVideoJuegos();
+       cantidadAccesorios = leerCantidadAccesorios();
+       cantidadTarjetas = leerCantidadTarjetas();
+       
 
 
 
+    } while (contadorClientes > numeroClientes);
+
+    return 0;
 }
 
 int leerNumeroClientes(){
     int numeroClientes;
-    cout << "Ingrese el numero de clientes: ";
+    cout << "Ingrese la cantidad de clientes: ";
     cin >> numeroClientes;
     return numeroClientes;
 }
 
-void mostrarMenuOpciones(){
-    
+int leerCantidadVideoJuegos(int numeroClientes){
+    int cantidadVideoJuegos;
+    cout << "Ingrese la cantidad de videojuegos: ";
+    cin >> cantidadVideoJuegos;
+    return cantidadVideoJuegos;
 }
 
-float calcularMonto(){
-
+int leerCantidadAccesorios(int numeroClientes){
+    int cantidadAccesorios;
+    cout << "Unidades vendidas de Accesorios: ";
+    cin >> cantidadAccesorios;
+    return cantidadAccesorios;
 }
 
-void mostrarResultado(){
+int leerCantidadTarjetas(int numeroTarjetas){
+    int cantidadTarjetas;
+    cout << "Unidades vendidas de Tarjetas Visuales: ";
+    cin >> cantidadTarjetas;
+    return cantidadTarjetas;
+}
 
+float calcularMonto(int cantidadVideoJuegos, int cantidadAccesorios, int cantidadTarjetas){
+    float montoTotal;
+    float videoJuegos = 170.50;
+    float accesorios = 80.90;
+    float tarjetas = 50.00;
+    montoTotal = (cantidadVideoJuegos * 170.50) + (cantidadAccesorios * 80.90) + (cantidadTarjetas * 50.00);
+    return montoTotal;
+}
+
+void reporteDeVentas(int uni_videojuegos, int uni_accesorios, int uni_tarjetas, float monto_total, int montoDescontado){
+    cout << "Total unidades de VideoJuegos: " << uni_videojuegos << endl;
+    cout << "Total unidades de accesorios: " << uni_accesorios << endl; 
+    cout << "Total unidades de tarjetas visuales: " << uni_tarjetas << endl;
+    cout << "Monto total facturado: " << monto_total << endl;
+    cout << "Monto descontado: " << montoDescontado << endl;
 }
--- C++/Ejercicio2.cpp
+++ C++/Ejercicio2.cpp
@@ -7,126 +7,124 @@ char leer_plataforma();
 char leer_nivel_satisfaccion();
 
 int main() {
+	int edad;
+	char sexo, plataforma, nivel_satisfaccion;
 
-    int edad;
-    char sexo, plataforma, nivel_satisfaccion;
-    int cont_estudiante = 0, cont_adultos =0, cont_estu_plat_canvas=0;
-    int cont_nivel_sat_bueno = 0, cont_nivel_sat_regular = 0, cont_nivel_sat_malo = 0 ;
-    int edad_mujer_joven=-1;
-    double promedio = 0;
+	int cont_estudiante = 0, cont_adultos =0, cont_estu_plat_canvas=0;
+	int cont_nivel_sat_bueno = 0, cont_nivel_sat_regular = 0, cont_nivel_sat_malo = 0 ;
+	int edad_mujer_joven=-1;
+	double promedio = 0;
 
-    do {
-    cout << "Estudiante " << (cont_estudiante+1) <<endl;
-    edad = leer_edad();
+	do {
+    	cout << "Estudiante " << (cont_estudiante+1) <<endl;
+    	edad = leer_edad();   	 
+    	if (edad == 0) {
+        	break;
+    	}
 
-    if (edad == 0) {
-     break;
-    }
+    	sexo = leer_sexo();
+    	plataforma = leer_plataforma();
+    	nivel_satisfaccion = leer_nivel_satisfaccion();
 
-    sexo = leer_sexo();
-    plataforma = leer_plataforma();
-    nivel_satisfaccion = leer_nivel_satisfaccion();
+    	if (edad >= 60) {
+        	cont_adultos++;
+    	}
 
-    if (edad >= 60) {
-    cont_adultos++;
-    }
+    	if (plataforma == 'C') {
+        	cont_estu_plat_canvas++;
+    	}
 
-    if (plataforma == 'C') {
-    cont_estu_plat_canvas++;
-     }
+    	if (nivel_satisfaccion == 'B') {
+        	cont_nivel_sat_bueno++;
+    	}
+    	else  if (nivel_satisfaccion == 'R') {
+        	cont_nivel_sat_regular++;
+    	}
+    	else  {
+        	cont_nivel_sat_malo++;
+    	}
 
-    if (nivel_satisfaccion == 'B') {
-    cont_nivel_sat_bueno++;
-    }
+    	if (sexo == 'F' && plataforma != 'M') {
+        	edad_mujer_joven = edad;
+    	}
 
-    else if (nivel_satisfaccion == 'R') {
-    cont_nivel_sat_regular++;
-    }
+    	cout << endl;
+    	cont_estudiante++;
 
-    else {
-    cont_nivel_sat_malo++;
-    }
+	} while (edad != 0);
 
-    if (sexo == 'F' && plataforma != 'M') {
-    edad_mujer_joven = edad;
-    }
+    
+	if (cont_estudiante > 0) {
+    	promedio = (double)cont_estu_plat_canvas / cont_estudiante;
+	}
 
-    cout << endl;
-    cont_estudiante++;
+	cout << "Resultados" << endl;
+	cout << "¿Cuantos estudiantes son adultos mayores (adulto mayor es quien tiene 60 o mas años)?: " << cont_adultos << endl;
+	cout << "¿Cual es el promedio de edad de los estudiantes que utilizan la plataforma Canvas?: " << promedio << endl;
+	cout << "¿Cual es el nivel o niveles de satisfaccion que tiene la menor frecuencia?" << endl;
+    
+	if (cont_nivel_sat_bueno <= cont_nivel_sat_regular && cont_nivel_sat_bueno <= cont_nivel_sat_malo) {
+    	cout << "Bueno" << endl;
+	}
 
-    } while (edad != 0);
+	if (cont_nivel_sat_regular <= cont_nivel_sat_bueno && cont_nivel_sat_regular <= cont_nivel_sat_malo) {
+    	cout << "Regular" << endl;
+	}
 
-    if (cont_estudiante > 0) {
-    promedio = (double)cont_estu_plat_canvas / cont_estudiante;
-    }
+	if (cont_nivel_sat_malo <= cont_nivel_sat_bueno && cont_nivel_sat_malo <= cont_nivel_sat_regular) {
+    	cout << "Malo" << endl;
+	}
 
-    cout << "Resultados" << endl;
-    cout << "¿Cuantos estudiantes son adultos mayores (adulto mayor es quien tiene 60o mas años)?: " << cont_adultos << endl;
-    cout << "¿Cual es el promedio de edad de los estudiantes que utilizan la plataforma Canvas?: " << promedio << endl;
-    cout << "¿Cual es el nivel o niveles de satisfaccion que tiene la menor frecuencia?" << endl;
+	cout << "¿Cual es la edad de la mujer mas joven que no utiliza Moodle?" << endl;
+	if (edad_mujer_joven != -1) {
+    	cout << "La edad es:" << edad_mujer_joven << endl;
+	}
+	else {
+    	cout << "No hubo" << endl;
+	}
 
-    if (cont_nivel_sat_bueno <= cont_nivel_sat_regular && cont_nivel_sat_bueno <=
-    cont_nivel_sat_malo) {
-    cout << "Bueno" << endl;
-    }
-
-    if (cont_nivel_sat_regular <= cont_nivel_sat_bueno && cont_nivel_sat_regular <=
-    cont_nivel_sat_malo) {
-    cout << "Regular" << endl;
-    }
-
-    if (cont_nivel_sat_malo <= cont_nivel_sat_bueno && cont_nivel_sat_malo <=
-    cont_nivel_sat_regular) {
-     cout << "Malo" << endl;
-    }
-
-    cout << "¿Cual es la edad de la mujer mas joven que no utiliza Moodle?" << endl;
-    if (edad_mujer_joven != -1) {
-    cout << "La edad es:" << edad_mujer_joven << endl;
-    }
-    else {
-    cout << "No hubo" << endl;
-    }
-     return 0;
-    }
-
-    int leer_edad() {
-    int edad;
-    do
-    {
-    cout << "Ingrese su edad:";
-    cin >> edad;
-    } while ((edad<17 || edad>65) && edad!=0);
-r   eturn edad;
+	return 0;
 }
+
+int leer_edad() {
+	int edad;
+	do
+	{
+    	cout << "Ingrese su edad:";
+    	cin >> edad;
+	} while ((edad<17 || edad>65) && edad!=0);
+	return edad;
+}
+
 char leer_sexo() {
-char sexo;
-do
-{
- cout << "Sexo(F:Femenino, M:Masculino):";
-cin >> sexo;
-sexo = toupper(sexo);
-} while (sexo !='F' && sexo!='M');
-return sexo;
+	char sexo;
+	do
+	{
+    	cout << "Sexo(F:Femenino, M:Masculino):";
+    	cin >> sexo;
+    	sexo = toupper(sexo);
+	} while (sexo !='F' && sexo!='M');
+	return sexo;
 }
+
 char leer_plataforma() {
-char plataforma;
-do
-{
- cout << "Plataforma(B:Blackboard, C:Canvas, M: Moodle):";
-cin >> plataforma;
-plataforma = toupper(plataforma);
-} while (plataforma != 'B' && plataforma != 'C' && plataforma != 'M');
-return plataforma;
+	char plataforma;
+	do
+	{
+    	cout << "Plataforma(B:Blackboard, C:Canvas, M: Moodle):";
+    	cin >> plataforma;
+    	plataforma = toupper(plataforma);
+	} while (plataforma != 'B' && plataforma != 'C' && plataforma != 'M');
+	return plataforma;
 }
+
 char leer_nivel_satisfaccion() {
-char nivel_satisfaccion;
-do
-{
- cout << "Nivel de satisfaccion (B:Bueno, R:Regular, M:Malo):";
-cin >> nivel_satisfaccion;
-nivel_satisfaccion = toupper(nivel_satisfaccion);
-} while (nivel_satisfaccion != 'B' && nivel_satisfaccion != 'R' && nivel_satisfaccion !=
-'M');
-return nivel_satisfaccion;
+	char nivel_satisfaccion;
+	do
+	{
+    	cout << "Nivel de satisfaccion (B:Bueno, R:Regular, M:Malo):";
+    	cin >> nivel_satisfaccion;
+    	nivel_satisfaccion = toupper(nivel_satisfaccion);
+	} while (nivel_satisfaccion != 'B' && nivel_satisfaccion != 'R' && nivel_satisfaccion != 'M');
+	return nivel_satisfaccion;
 }
