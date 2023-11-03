24fcb2c8b2a9db396d9025fd70bdede4545c761c
SergioDevEngineer <132532322+SergioCodeGX@users.noreply.github.com>
2023-11-03 13:43:14 +0000

    cambios

--- C++/Arreglos.cpp
+++ C++/Arreglos.cpp
@@ -1,65 +1,108 @@
-//Ejercicio 1
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+
 using namespace std;
 
-// Función para crear un arreglo dinámico
-void crearArreglo(int *&arr, int tamano) {
-	arr = new int[tamano];
+struct Paciente {
+    int edad;
+    char sexo;
+    char tipoPrueba;
+    char resultado;
+};
+
+void genera_y_muestra_arreglo(Paciente* pacientes, int n);
+void hallar_menor_y_cuantos(Paciente* pacientes, int n);
+void porcentaje_resultados(Paciente* pacientes, int n);
+void promedio_edad_por_tipo(Paciente* pacientes, int n);
+void ordenar_y_mostrar(Paciente* pacientes, int n);
+
+int main()
+{
+    srand(time(0));
+    int n = 5;
+    Paciente* pacientes = new Paciente[n];
+
+    genera_y_muestra_arreglo(pacientes, n);
+    hallar_menor_y_cuantos(pacientes,n);
+
+
+    delete[] pacientes;
+    system("pause>0");
+    return 0;
 }
 
-// Función para ingresar números en el arreglo
-void ingresarNumeros(int *arr, int tamano) {
-	cout << "Ingrese los números uno por uno:" << endl;
-	for (int i = 0; i < tamano; i++) {
-    	cout << "Número " << i + 1 << ": ";
-    	cin >> arr[i];
-	}
+void genera_y_muestra_arreglo(Paciente* pacientes, int n) {
+    for (int i = 0; i < n; i++)
+    {
+        //generar valores aleatorios
+        pacientes[i].edad = rand() % 76 + 15;
+        pacientes[i].sexo = (rand() % 2 == 0)? 'M': 'F';
+        pacientes[i].tipoPrueba = (rand() % 3 == 0) ? 'P'
+            : (rand() % 2 == 0) ? 'A' : 'S';
+        pacientes[i].resultado = (rand() % 2 == 0) ? 'P' : 'N';
+
+        //mostrar los resultados
+        cout << "============================================" << endl;
+        cout << "Paciente " << (i + 1) << ":" << endl;
+        cout << "Edad " << pacientes[i].edad << ":" << endl;
+        cout << "Sexo " << pacientes[i].sexo << ":" << endl;
+        cout << "Tipo Prueba " << pacientes[i].tipoPrueba << ":" << endl;
+        cout << "Resultado " << pacientes[i].resultado << ":" << endl;
+        cout << "============================================" << endl;
+
+    }
 }
 
-// Función para imprimir elementos únicos
-void imprimirElementosUnicos(int *arr, int tamano) {
-	// Crear un arreglo dinámico para realizar un seguimiento de elementos únicos
-	int *elementosUnicos = new int[tamano];
-	int elementosUnicosTamano = 0;
+void hallar_menor_y_cuantos(Paciente* pacientes, int n) {
+    int menorEdad = pacientes[0].edad;
+    int contMenorEdad = 0;
 
-	// Iterar a través del arreglo
-	for (int i = 0; i < tamano; i++) {
-    	bool esRepetido = false;
-    	// Verificar si el elemento ya se ha impreso
-    	for (int j = 0; j < elementosUnicosTamano; j++) {
-        	if (arr[i] == elementosUnicos[j]) {
-            	esRepetido = true;
-            	break;
-        	}
-    	}
+    for (int i = 0; i < n; i++)
+    {
+        if (pacientes[i].edad < menorEdad) {
+            menorEdad = pacientes[i].edad;
+            contMenorEdad=1;
+        }else if (pacientes[i].edad ==  menorEdad) {
+            contMenorEdad++;
+        }
+    }
 
-    	// Si el elemento no es repetido, imprimirlo y agregarlo al arreglo de elementos únicos
-    	if (!esRepetido) {
-        	cout << "Elemento único: " << arr[i] << endl;
-        	elementosUnicos[elementosUnicosTamano] = arr[i];
-        	elementosUnicosTamano++;
-    	}
-	}
-
-	// Liberar la memoria del arreglo dinámico de elementos únicos
-	delete[] elementosUnicos;
+    cout << "La menor edad de los pacientes es:" << menorEdad << endl;
+    cout << "Cantidad de pacientes con esas edad:" << contMenorEdad << endl;    
 }
 
-int main() {
-	int tamano;
-	cout << "Ingrese la cantidad de números que desea ingresar: ";
-	cin >> tamano;
+void porcentaje_resultados(Paciente* pacientes, int n) {
+    int contMenores35 = 0, contMayores50 = 0;
+    double porMenores35, porMayores50;
 
-	int *numeros;
-	crearArreglo(numeros, tamano); // Llama a la función para crear el arreglo dinámico
+    //Incremento de contadores
+    for (int i = 0; i < n; i++)
+    {
+        if (pacientes[i].sexo == 'M' && pacientes[i].edad < 35 && pacientes[i].resultado == 'P') {
+            contMenores35++;
+        }
 
-	ingresarNumeros(numeros, tamano); // Llama a la función para ingresar números en el arreglo
+        if (pacientes[i].edad >= 50 && pacientes[i].resultado == 'N') {
+            contMayores50++;
+        }
+    }
 
-	cout << "Elementos únicos:" << endl;
-	imprimirElementosUnicos(numeros, tamano); // Llama a la función para imprimir elementos únicos
+    //Calcular los porcentajes
+    porMenores35 = (contMenores35 / n) * 100;
+    porMayores50 = (contMayores50 / n) * 100;
 
-	// Liberar la memoria del arreglo dinámico principal
-	delete[] numeros;
+    //Mostrar resultados
+    cout << "Porcentajes de varones menores de 35 " <<porMenores35 <<endl;
+    cout << "Porcentajes de personas mayores de 50 " << porMayores50 << endl;
 
-	return 0;
+}
+
+void promedio_edad_por_tipo(Paciente* pacientes, int n) {
+
+}
+
+void ordenar_y_mostrar(Paciente* pacientes, int n) {
+	
+
 }
